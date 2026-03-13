fbbd22524748aa3ded483dbfe7638ddb74fe2e17
trangvo6652938-tech <trangvo6652938@gmail.com>
2026-03-14 05:32:26 +0700

    Update app.py

--- math_ai_solver/app.py
+++ math_ai_solver/app.py
@@ -172,8 +172,6 @@ def home():
     )
 import os
 if __name__ == "__main__":
-
-    port = int(os.environ.get("PORT",10000))
-
-    app.run(host="0.0.0.0", port=port)
+        port =int(os.environ.get("PORT",10000))
+        app.run(host="0.0.0.0", port=port)
            
